bd8e45df83c0f9cb69643b3832b08af5ae24eb6c
Khaled alshammari <eng.Khaled10@outlook.com>
2024-08-22 02:07:14 +0300

    done

--- src/Componants/TextsPage.js
+++ src/Componants/TextsPage.js
@@ -1,7 +1,8 @@
 import React from "react";
-import money from "../assets/media/money-42.png";
+import money from "../assets/media/money png.jpg";
 import "../assets/css/Style.css";
 import { useState } from "react";
+import bank from "../assets/media/image.png";
 
 const TextsPage = () => {
   const [count, setCount] = useState(0);
@@ -9,18 +10,37 @@ const TextsPage = () => {
   const handleButton = () => {
     setCount(count + 1);
   };
-  const changeCount = (e) => {
+  let changeCount = (e) => {
+    setCount(e.target.value);
+  };
+
+  // const [counter, setCounter] = useState(100);
+
+  // const handleBank = () => {
+  //   setCounter(counter + 1);
+  //   if (counter == 100) {
+  //     return counter + 2;
+  //   } else if (counter == 150) {
+  //     return counter + 3;
+  //   }
+  // };
+  // console.log(counter);
+  let changeCounter = (e) => {
     setCount(e.target.value);
   };
   console.log(count);
   return (
     <div>
+      <div class="bank">
+        <h1>
+          <img src={bank} class="bankImg" /> X{count}$
+        </h1>
+      </div>
       <div class="container">
-        <h1>money collected {count}$</h1>
+        <h2>money collected {count}$</h2>
         <button onClick={handleButton}>
           <img src={money} class="img" />
         </button>
-        <h2></h2>
       </div>
     </div>
   );
